c202dee50e2a5a7e05e9f09d23d579a707e9cf6c
Michael <scocco.michael87@gmail.com>
2021-05-10 11:41:53 +0200

    blocco 4

--- js-blocco-4/js-blocco-4.js
+++ js-blocco-4/js-blocco-4.js
@@ -1,12 +1,20 @@
 /**********************************
 FARE NUOVA CARTELLA SNAKS BLOCCO 4*
 **********************************/
+
+/*******
+SNAKS-1*
+*******/
 /*
 creare un oggetto palla che abbia le seguenti proprietà.
 nome = palla;
 peso = 10;
 */
-var pallaOgg = {
+var PallaOgg = {
     nome : 'palla',
     peso : 10,
 };
+
+/*******
+SNAKS-2*
+*******/
